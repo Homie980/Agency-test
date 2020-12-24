f5e1af9bc8521567200be1255b15a0b8a1aaf902
Abdelrahman Khaled <boodykhaled@gmail.com>
2020-12-24 17:57:23 +0200

    eighth commit fixed theme issue-v4.0

--- src/components/layout.js
+++ src/components/layout.js
@@ -83,7 +83,7 @@ const Layout = ({ children }) => {
   const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
-    <ThemeProvider theme={currentTheme === "dark" ? darkTheme : darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
       <Cursor toggleMenu={toggleMenu} />
       <Header
